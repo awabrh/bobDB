3c9c28c799c5636f0cd66dd5a2395b280d61ed5d
Awab Rhamtalla <awabrh@gmail.com>
2024-04-20 01:45:48 +0300

    create a temp file then rename it

--- main.go
+++ main.go
@@ -2,23 +2,34 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 )
 
 func SaveData(path string, data []byte) error {
-	fp, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0664)
+	tmp := fmt.Sprintf("%s.tmp.%d", path, rand.Int())
+	fp, err := os.OpenFile(tmp, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0664)
 	if err != nil {
 		return err
 	} 
-
-	defer fp.Close()
-
+	
 	_, err = fp.Write(data)
-	return err
+	if err != nil {
+		os.Remove(tmp)
+		return err
+	}
+	
+	fp.Close()
+	
+	return os.Rename(tmp, path)
 }
 
 func main() {
-	data := "Hello, again!"
-	SaveData("./out/test.txt", []byte(data))
-	fmt.Println(data)
+	data := "Hello?!"
+	err := SaveData("./output/text.txt", []byte(data))
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Success!")
+	}
 }
