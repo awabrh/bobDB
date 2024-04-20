bf0ae69ad502b4f27427d200aa9ba7332c61884c
Awab Rhamtalla <awabrh@gmail.com>
2024-04-20 03:00:00 +0300

    append only log

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"fmt"
 	"math/rand"
 	"os"
@@ -18,17 +19,43 @@ func SaveData(path string, data []byte) error {
 		os.Remove(tmp)
 		return err
 	}
+
+	err = fp.Sync()
+	if err != nil {
+		os.Remove(tmp)
+		return err
+	}
 	
 	fp.Close()
 	
 	return os.Rename(tmp, path)
 }
 
-func main() {
-	data := "Hello?!"
-	err := SaveData("./output/text.txt", []byte(data))
+func LogCreate(path string) (*os.File, error) {
+	return os.OpenFile(path, os.O_RDWR|os.O_APPEND, 0664)
+}
+
+func LogAppend(fp *os.File, line string) error {
+	buf := []byte(line)
+	buf = append(buf, '\n')
+	_, err := fp.Write(buf)
 	if err != nil {
-		fmt.Println(err)
+		return err
+	}
+
+	return fp.Sync()
+}
+
+func main() {
+	data := "Hello, World!"
+	fp, err := LogCreate("./output/text.txt")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	err = LogAppend(fp, data)
+	if err != nil {
+		log.Fatal(err)
 	} else {
 		fmt.Println("Success!")
 	}
